c7d33f3d9e0f0cb349dc14e7c7177efb5f858f37
DiegoDSlo <diego.slowing@gmail.com>
2024-04-09 15:27:29 -0600

    fix: added parameters to the cards

--- src/card.jsx
+++ src/card.jsx
@@ -1,16 +1,14 @@
 import './card.css'
 
-function Card(){
-    return(
-        <>
-            <li>
-                <img src="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png" alt="Descripción de la imagen" class = 'small'></img>
-                <div class="titulo">Producto 1</div>
-                <div class="precio">Q20.00</div>
-                <button class='moreinfo'> Más Infromación </button>
-            </li>
-        </>
-    )
+function Card({ nombre, precio, imagen }) {
+    return (
+        <li>
+            <img src={imagen} alt="Descripción de la imagen" className='small' />
+            <div className="titulo">{nombre}</div>
+            <div className="precio">{precio}</div>
+            <button className='moreinfo'> Más Información </button>
+        </li>
+    );
 }
 
 export default Card 
