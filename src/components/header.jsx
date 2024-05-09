3b58ce590868fbb79e775ef6acb1736e8f420487
DiegoDSlo <diego.slowing@gmail.com>
2024-05-08 19:35:11 -0600

    feat: added compontents folder

---
+++ src/components/header.jsx
@@ -0,0 +1,21 @@
+import './header.css'
+import userIcon from '../image/userIcon.png';
+import notificationIcon from '../image/notificationIcon.png';
+import cartIcon from '../image/cartIcon.png';
+
+function Header({ nombre, precio, imagen }) {
+
+    return (
+        <header className="fixed-header">
+        <div className="brand">AGUATESA</div>
+        <div style={{marginRight: '20px'}} className="icons">
+          {/* Cart, User, and Notification icons */}
+          <img src={cartIcon} alt="Cart" />
+          <img src={userIcon} alt="User" />
+          <img src={notificationIcon} alt="Notifications" />
+        </div>
+      </header>
+    );
+}
+
+export default Header
