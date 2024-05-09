5d7cae6a651748ca0f66d6dee6b9179602b957f0
DiegoDSlo <diego.slowing@gmail.com>
2024-05-08 20:45:54 -0600

    feat: added services pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import './styles.css'
 import menuIcon from './image/menuIcon.png';
 import Card from './components/card'
 import Header from './components/header'
+import BombasAgua from './pages/products/bombasagua'
+import Mantenimiento from './pages/services/mantenimiento'
+import Perforacion from './pages/services/perforacion'
 
 
 function App() {
@@ -16,16 +19,7 @@ function App() {
       <div className="fixed-section">
           <img style={{ top: '20px',height:'20px', width:'30px'}} src={menuIcon} alt="menu" />
       </div>
-      <main className="main-content">
-        <h2> Tipo de Producto</h2>
-        <ul>
-          <Card nombre = 'Producto 1' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
-          <Card nombre = 'Producto 2' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
-          <Card nombre = 'Producto 3' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
-          <Card nombre = 'Producto 4' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
-          <Card nombre = 'Producto 5' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
-        </ul>
-      </main>
+      <Perforacion></Perforacion>
       <footer>
         Todos los Derechos Reservados a Aguatesa 2024
       </footer>
---
+++ src/pages/products/bombasagua.jsx
@@ -0,0 +1,19 @@
+import Card from "../../components/card";
+
+function BombasAgua() {
+
+    return (
+        <main className="main-content">
+            <h2> Tipo de Producto</h2>
+            <ul>
+                <Card nombre = 'Producto 1' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
+                <Card nombre = 'Producto 2' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
+                <Card nombre = 'Producto 3' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
+                <Card nombre = 'Producto 4' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
+                <Card nombre = 'Producto 5' precio = 'Q.20.00' imagen="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png"/>
+            </ul>
+      </main>
+    );
+}
+
+export default BombasAgua
