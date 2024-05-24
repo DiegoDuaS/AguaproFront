b01f9201f6aaef0fb41a03fd77d69e5d6efde380
DiegoDSlo <diego.slowing@gmail.com>
2024-05-23 18:21:54 -0600

    feat: added the left-section of the large cards

--- src/components/LargeCard.jsx
+++ src/components/LargeCard.jsx
@@ -26,7 +26,15 @@ const LargeCard = ({ isOpen, closeCard }) => {
 
   return (
     <div className="large-card" ref={cardRef}>
-      <button className="close-button" onClick={closeCard}>x</button>
+      <div className="left-section">
+        <img src="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png" alt="Descripción de la imagen" className='image-maxcard' />
+        <div className="brand">Nombre del Producto</div>
+        <p className='price'>Q.1000.00</p>
+      </div>
+      <button className="close-button" onClick={closeCard}>X</button>
+      <div className="right-section">
+    
+      </div>
     </div>
   );
 };
