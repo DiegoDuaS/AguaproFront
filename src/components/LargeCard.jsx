b3bef4338fb4538b72a8e448512afaaf2f8d45ee
DiegoDSlo <diego.slowing@gmail.com>
2024-05-24 11:25:49 -0600

    feat: components of large card

--- src/components/LargeCard.jsx
+++ src/components/LargeCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './LargeCard.css'; // Import your CSS file
+import { IoIosArrowDown } from "react-icons/io";
 
 const LargeCard = ({ isOpen, closeCard }) => {
   const cardRef = useRef(null);
@@ -29,9 +30,11 @@ const LargeCard = ({ isOpen, closeCard }) => {
     <div className="large-card" ref={cardRef}>
       <button className="close-button" onClick={closeCard}>X</button>
       <div className="left-section">
-        <img src="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png" alt="Descripción de la imagen" className='image-maxcard' />
-        <div className="brand">Nombre del Producto</div>
-        <p className='price'>Q.1000.00</p>
+        <div className='photo-title'>
+          <img src="https://images.squarespace-cdn.com/content/v1/5b60a97de7494070b92f2702/1633103417460-HSYJWDNDGRI5GC5PHXS3/aguatesa.png" alt="Descripción de la imagen" className='image-maxcard' />
+          <div className="product-title">Bomba Sumergible de Acero Inoxidable</div>
+          <p className='price'>Q.1000.00</p>
+        </div>
       </div>
       <div className="right-section">
         <div className="infobox">
@@ -56,7 +59,12 @@ const LargeCard = ({ isOpen, closeCard }) => {
             </div>
         </div>
         <div className="add-to-cart"> 
+          <div className='cuantity-box'>
+            1
+            <IoIosArrowDown />
+          </div>
 
+          <button className='add'>Agregar al Carrito</button>
         </div>
       </div>
     </div>
