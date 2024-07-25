f35af1ab53a4c999f1939afb2cdaa6393573af5a
DiegoDSlo <diego.slowing@gmail.com>
2024-07-25 15:13:28 -0600

    fix: fixed cards subtitles

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,7 +5,7 @@ function Card({ nombre, precio, imagen, onMoreInfoClick }) {
         <li className='smallcards'>
             <img src={imagen} alt="Descripción de la imagen" className='small' />
             <div className="titulo">{nombre}</div>
-            <div className="precio">{precio}</div>
+            <div className="precios">{precio}</div>
             <button className='moreinfo' onClick={onMoreInfoClick}> Más Información </button>
         </li>
     );
