ec6712a375343e08dbac60f76f935706f9014576
DiegoDuaS <diego.slowing@gmail.com>
2024-11-04 19:04:26 -0600

    fix: resolved conflicts for merge

--- src/components/cards/infoProdCard.jsx
+++ src/components/cards/infoProdCard.jsx
@@ -71,19 +71,11 @@ const InfoProdCard = ({ isOpen, closeCard, product}) => {
                         </div>
                         <div className="table-row2">
                             <div className="table-cell title">Tipo de Producto</div>
-<<<<<<< HEAD
                             <div className="table-cell info_des_prod">{product.tipoproducto}</div>
                         </div>
                         <div className="table-row2">
                             <div className="table-cell title">Precio</div>
                             <div className="table-cell info_des_prod">Q.{product.precio}</div>
-=======
-                            <div className="table-cell info_des_prod">{product.tipo_producto}</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Precio</div>
-                            <div className="table-cell info_des_prod">${product.precio}</div>
->>>>>>> testing
                         </div>
                         <div className="table-row2">
                             <div className="table-cell title">Disponibilidad</div>
@@ -98,38 +90,16 @@ const InfoProdCard = ({ isOpen, closeCard, product}) => {
                             <div className="table-cell info_des_prod">{product.material}</div>
                         </div>
                         <div className="table-row2">
-<<<<<<< HEAD
                             <div className="table-cell title">Capacidad Mínima</div>
                             <div className="table-cell info_des_prod">{product.capacidadmin}</div>
                         </div>
                         <div className="table-row2">
                             <div className="table-cell title">Capacidad Máxima</div>
                             <div className="table-cell info_des_prod">{product.capacidadmax}</div>
-=======
-                            <div className="table-cell title">Profundidad</div>
-                            <div className="table-cell info_des_prod">{product.profundidad} mm</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Temperatura Líquida Máxima</div>
-                            <div className="table-cell info_des_prod">{product.temperatura_liquida_max} °C</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Conexión de Tubería</div>
-                            <div className="table-cell info_des_prod">{product.conexion_tuberia}</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Presión Funcional</div>
-                            <div className="table-cell info_des_prod">{product.presion_funcional} bar</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Head</div>
-                            <div className="table-cell info_des_prod">{product.head} m</div>
->>>>>>> testing
                         </div>
                     </div>
                 </div>
                 <div className='section'>
-<<<<<<< HEAD
                     <div className="table-cell title">Imagen del Producto</div>
                     {isLoading ? (
                         <>
@@ -141,54 +111,6 @@ const InfoProdCard = ({ isOpen, closeCard, product}) => {
                     ) : (
                      <img className='img_prod2' src={imageSrc} alt='Bomba de agua' />
                      )}
-=======
-                    <div className='vertical-table'>
-                        <div className="table-row2">
-                            <div className="table-cell title">Aplicaciones</div>
-                            <div className="table-cell info_des_prod">{product.aplicaciones}</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Temperatura Media</div>
-                            <div className="table-cell info_des_prod">{product.temperatura_media} °C</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">GPM Mínimo</div>
-                            <div className="table-cell info_des_prod">{product.min_gpm} GPM</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">GPM Máximo</div>
-                            <div className="table-cell info_des_prod">{product.max_gpm} GPM</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">HP Mínimo</div>
-                            <div className="table-cell info_des_prod">{product.min_hp} HP</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">HP Máximo</div>
-                            <div className="table-cell info_des_prod">{product.max_hp} HP</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Capacitor</div>
-                            <div className="table-cell info_des_prod">{product.capacitor}</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Temperatura Líquida Mínima</div>
-                            <div className="table-cell info_des_prod">{product.temperatura_liquida_min} °C</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Temperatura Ambiente</div>
-                            <div className="table-cell info_des_prod">{product.temperatura_ambiente} °C</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Presión</div>
-                            <div className="table-cell info_des_prod">{product.presion} bar</div>
-                        </div>
-                        <div className="table-row2">
-                            <div className="table-cell title">Caudal</div>
-                            <div className="table-cell info_des_prod">{product.flow_rate} L/min</div>
-                        </div>
-                    </div>
->>>>>>> testing
                 </div>
             </div>
 
--- src/components/forms/formsSer.jsx
+++ src/components/forms/formsSer.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './formsSer.css';
 import { Button } from 'react-bootstrap';
-<<<<<<< HEAD
 import useRegisterSolicitud from '../../hooks/useRegisterSolicitud';
 import useSolicitud from '../../hooks/email/useSolicitud';
 
@@ -197,85 +196,6 @@ const FormsSer = ({type, setShowForms, setSuccessMessage, setErrorMessage}) => {
         );
     }
     return null;
-=======
-
-const FormsSer = ({type}) => {
-    const [name, setName] = useState("");
-    const [correo, setCorreo] = useState("");
-    const [q1, setQ1] = useState("");
-    const [q2, setQ2] = useState("");
-    const [q3, setQ3] = useState("");
-    const [q4, setQ4] = useState("");
-
-    return (
-        <div className="forms">
-            <div className='persoinfo'>
-                <div className='inputsect'>
-                    <label className='labelforms_ser'><strong>Nombre Completo:</strong></label>
-                    <input
-                        type="text" 
-                        className="forms_input personalinfo"
-                        value={name}
-                        placeholder="Nombre"
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <div className='inputsect'>
-                    <label className='labelforms_ser'><strong>Correo Electrónico:</strong></label>
-                    <input
-                        type="email" 
-                        className="forms_input personalinfo"
-                        value={correo}
-                        placeholder="Correo Electrónico"
-                        onChange={(e) => setCorreo(e.target.value)}
-                    />
-                </div>
-            </div>
-            
-            <div className='inputsect'>
-                <label className='labelforms_ser2'><strong>P1:</strong></label>
-                <input
-                    type="text" 
-                    className="forms_input"
-                    value={q1}
-                    placeholder="Pregunta 1"
-                    onChange={(e) => setQ1(e.target.value)}
-                />
-            </div>
-            <div className='inputsect'>
-                <label className='labelforms_ser2'><strong>P2:</strong></label>
-                <input
-                    type="text" 
-                    className="forms_input"
-                    value={q2}
-                    placeholder="Pregunta 2"
-                    onChange={(e) => setQ2(e.target.value)}
-                />
-            </div>
-            <div className='inputsect'>
-                <label className='labelforms_ser2'><strong>P3:</strong></label>
-                <input
-                    type="text" 
-                    className="forms_input"
-                    value={q3}
-                    placeholder="Pregunta 3"
-                    onChange={(e) => setQ3(e.target.value)}
-                />
-            </div>
-            <div className='inputsect'>
-                <label className='labelforms_ser2'><strong>P4:</strong></label>
-                <input
-                    type="text" 
-                    className="forms_input"
-                    value={q4}
-                    placeholder="Pregunta 4"
-                    onChange={(e) => setQ4(e.target.value)}
-                />
-            </div>
-            <button className='sendforms'>Enviar</button>
-        </div>
-    );
->>>>>>> testing
 };
 
 export default FormsSer;
